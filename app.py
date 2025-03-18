2282de96ba2ede061d9247d9505b07c8421ec36f
Lalit sharma <32238406+lalitsee49@users.noreply.github.com>
2025-03-18 14:30:05 +0530

    final_changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from modules.utils import st
 from modules.whatif_scn1 import scn1
 from modules.whatif_scn2 import scn2
-# from modules.whatif_scn3 import scn3
+from modules.whatif_scn3 import scn3
 from modules.whatif_scn4 import scn_4
 from modules.whatif_scn5 import scn_5
 from modules.whatif_scn6_7 import scn6_7
@@ -12,7 +12,7 @@ from modules.whatif_scn6_7 import scn6_7
 SCENARIOS = {
     "Scenario 1": scn1,
     "Scenario 2": scn2,
-    # "Scenario 3": scn3,
+    "Scenario 3": scn3,
     "Scenario 4": scn_4,
     "Scenario 5": scn_5,
     "Scenario 6": scn6_7,
