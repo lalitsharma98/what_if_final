c456751c29eaa6038d2ff33a592037a3094e7a11
Lalit sharma <32238406+lalitsee49@users.noreply.github.com>
2025-06-24 15:33:54 +0530

    changes2 in app4.py

--- app4.py
+++ app4.py
@@ -17,7 +17,7 @@ from firebase_admin import credentials, firestore
 
 # --- Firebase setup using secrets ---
 if not firebase_admin._apps:
-    firebase_config = st.Secrets["firebase"]
+    firebase_config = st.secrets["firebase"]
     
     cred = credentials.Certificate({
         "type": firebase_config["type"],
